6581f15a2822d6a1d7d70a54b7256a867717e65f
Nicholas Contreras <nicholasc2000@yahoo.com>
2016-04-15 14:58:34 -0700

    scary maze

--- src/day5/StarShow.java
+++ src/day5/StarShow.java
@@ -13,36 +13,38 @@ public class StarShow {
 	Robot robot = new Robot("batman");
 
 	void makeStars() {
-
-		drawStar(150); // 5. delete this line. you will draw the star again in step 8.
+		robot.setSpeed(8);
 		// 13. Set the speed to 8
 
 		// 6. Make a variable to hold the X position of the Robot and set it to 10
-
+		int robotX = 10;
 		// 7. Make a variable to hold the Y position of the Robot and set it to 950
-
+		int robotY = 950;
 		// 8. Make a variable to hold the star size and set it to 25
+		int starSize = 25;
 
 		// 12. Repeat the steps #19 to #18, 30 times
+		for (int i = 0; i < 30; i++) {
+			robot.turn(12);
+			robot.setRandomPenColor();
+			// 19. Set the pen width to i
+			robot.setPenWidth(i);
+			// 10. Set the X position of the robot to your X variable
+			robot.setX(robotX);
+			// 11. Set the Y position of the robot to your Y variable
+			robot.setY(robotY);
+			// 9. Call the drawStar() method with your star size variable
+			drawStar(starSize);
+			// 14. Increase the X position by star size. See Figure 2.
+			robotX = robotX + starSize;
+			// 15. decrease the Y position by star size. See Figure 3.
+			robotY = robotY - starSize;
+			// 16. Increase the star size by 20
+			starSize = starSize + 15;
+			// 17. Turn the robot 12 degrees
 
-		// 19. Set the pen width to i
-
-		// 10. Set the X position of the robot to your X variable
-
-		// 11. Set the Y position of the robot to your Y variable
-
-		// 9. Call the drawStar() method with your star size variable
-
-		// 14. Increase the X position by star size. See Figure 2.
-
-		// 15. decrease the Y position by star size. See Figure 3.
-
-		// 16. Increase the star size by 20
-
-		// 17. Turn the robot 12 degrees
-
-		// 18. Make each star a different random color like in Figure 4.
-
+			// 18. Make each star a different random color like in Figure 4.
+		}
 	}
 
 	private void drawStar(int starSize) {
